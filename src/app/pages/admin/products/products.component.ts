3df22e58301e798bcca24a26face259f308357ba
Gianfranco <barriosgianfranco@gmail.com>
2024-04-22 20:36:21 -0600

    modificacion del apartado para agregar categoria a los productos nuevos

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ProductService } from '../services/product/product.service';
 
 @Component({
   selector: 'app-products',
@@ -9,20 +10,39 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
   
   isSidePanelVisible: boolean = false;
   productObj: any = {
-      "ProductId": 0,
-      "ProductSku": "",
-      "ProductName": "",
-      "ProductPrice": 0,
-      "ProductShortName": "",
-      "ProductDescription": "",
-      "CreatedDate": new Date(),
-      "DeliveryTimeSpan": "",
-      "CategoryId": 0,
-      "ProductImageUrl": "" 
+      "productId": 0,
+      "productSku": "",
+      "productName": "",
+      "productPrice": 0,
+      "productShortName": "",
+      "productDescription": "",
+      "createdDate": new Date(),
+      "deliveryTimeSpan": "",
+      "categoryId": 0,
+      "productImageUrl": "" 
+  };
+  categoryList: any [] = [];
+
+ constructor(private productSrv: ProductService){
+
+ }
+  ngOnInit(): void {
+    this.getALlCategory();
+  }
+
+  getALlCategory(){
+    this.productSrv.getCategory().subscribe(
+      (res:any) => {
+        this.categoryList = res.data;
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
   }
   openSidePanel (){
     this.isSidePanelVisible = true;
